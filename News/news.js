a4082ed2ca2ba9b8678644739fb2d4e36f4004a8
momentumsociety <131333042+momentumsociety@users.noreply.github.com>
2023-05-13 21:32:37 +0800

    Add files via upload

---
+++ News/news.js
@@ -0,0 +1,55 @@
+var globalbutton = document.getElementById("global-er-button");
+var flyout = document.getElementById("global-flyout-background");
+var htmlTag = document.documentElement;
+
+let isExpanded = false;
+
+globalbutton.addEventListener("click", function(){
+    if (isExpanded) {
+        flyout.classList.remove("flyout-open");
+        globalbutton.textContent = "Expand";
+        htmlTag.classList.remove("global-nav-noscroll");
+        isExpanded = false;
+    } else {
+        flyout.classList.add("flyout-open");
+        globalbutton.textContent = "Retract";
+        htmlTag.classList.add("global-nav-noscroll");
+        isExpanded = true;
+    }
+});
+
+let imageIndex = 1;
+showImages(imageIndex);
+
+function nextImages(n){
+    showImages(imageIndex += n);
+}
+
+function currentImage(n) {
+    showImages(imageIndex = n);
+}
+
+function showImages(n){
+    let i;
+    let images = document.getElementsByClassName("gallery-item");
+    let dots = document.getElementsByClassName("dot-item");
+    
+    if (n > images.length){
+        imageIndex = 1
+    }
+
+    if (n < 1){
+        imageIndex = images.length
+    }
+
+    for (i = 0; i < images.length; i++){
+        images[i].className = images[i].className.replace(" current","");
+    }
+
+    for (i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(" current","");
+    }
+
+    images[imageIndex-1].className += " current";
+    dots[imageIndex-1].className += " current";
+}
